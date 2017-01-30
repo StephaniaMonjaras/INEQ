7ad4e5d1b95057cc6b89277c9aa3eb7299bd25eb
Stephania <Stephania@DESKTOP-B08B6EE>
2017-01-30 10:03:04 -0600

    Se creo la estructura de programacion en capas para la solucion de INEQ

--- INEQ/WindowsApp/Form1.Designer.cs
+++ INEQ/WindowsApp/Form1.Designer.cs
@@ -28,20 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnInicioSesion = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicio.Location = new System.Drawing.Point(215, 61);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(183, 17);
+            this.lblInicio.TabIndex = 0;
+            this.lblInicio.Text = "Iniciar sesion con su cuenta";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(218, 179);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(196, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(218, 214);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(196, 20);
+            this.txtContraseña.TabIndex = 2;
+            // 
+            // btnInicioSesion
+            // 
+            this.btnInicioSesion.BackColor = System.Drawing.Color.Blue;
+            this.btnInicioSesion.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnInicioSesion.FlatAppearance.BorderSize = 0;
+            this.btnInicioSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicioSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicioSesion.ForeColor = System.Drawing.Color.White;
+            this.btnInicioSesion.Location = new System.Drawing.Point(265, 294);
+            this.btnInicioSesion.Name = "btnInicioSesion";
+            this.btnInicioSesion.Size = new System.Drawing.Size(99, 36);
+            this.btnInicioSesion.TabIndex = 3;
+            this.btnInicioSesion.Text = "Iniciar Sesion";
+            this.btnInicioSesion.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(287, 261);
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(671, 444);
+            this.Controls.Add(this.btnInicioSesion);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblInicio);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Button btnInicioSesion;
     }
 }
 
