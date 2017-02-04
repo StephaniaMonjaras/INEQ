8aab61f5b2e7890bc54a0e08dde1cf4711e0af4b
Stephania <Stephania@DESKTOP-B08B6EE>
2017-02-03 22:26:40 -0600

    Se agrega la clase Component Type y sus atributos correspondientes

---
+++ INEQ/Entities/Component.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Component
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public int ComponentTypeId { get; set; }
+        public int EquipmentId { get; set; }
+        public int EquipmentTypeId { get; set; }
+
+    }
+}
