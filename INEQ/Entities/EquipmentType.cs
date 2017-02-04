eda93fe6704d4052dab9f6ed129dbfd92c3d0e60
Stephania <Stephania@DESKTOP-B08B6EE>
2017-02-04 12:57:57 -0600

    Se realiza la tabla EquipmentType con susrespectivos atributos

---
+++ INEQ/Entities/EquipmentType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class EquipmentType
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public float UseFulLife { get; set; }
+        public float GuaranteeDuration { get; set; }
+        public bool Active { get; set; }
+    }
+}
