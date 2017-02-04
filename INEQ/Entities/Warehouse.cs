9b8312c26038384d31f4beb9c001ad5986299955
Stephania <Stephania@DESKTOP-B08B6EE>
2017-02-04 13:14:48 -0600

    se crea la tabla Warehouse con sus respectivos atributos

---
+++ INEQ/Entities/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Warehouse
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string IS { get; set; }
+        public string Responsable { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
