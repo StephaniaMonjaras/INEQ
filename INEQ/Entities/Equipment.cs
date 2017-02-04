0af74c1390c50a915e1331b2845b79db3c80efa1
Stephania <Stephania@DESKTOP-B08B6EE>
2017-02-03 22:42:09 -0600

    Se realiza la clase Equipment con sus respectivos atributos

---
+++ INEQ/Entities/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Equipment
+    {
+        public int id { get; set; }
+        public int EquipmentTypeId { get; set; }
+        public int ModelId { get; set; }
+        public int BrandId { get; set; }
+        public int StatusId { get; set; }
+        public int WarehouseId { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Serie { get; set; }
+        public string SofttekId { get; set; }
+        public bool Active { get; set; }
+    }
+}
